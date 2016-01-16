446ba39b92681c8f10f147a5dc9b6515f0328af4
Peter Stromhaug <pas282@cornell.edu>
2016-01-16 18:59:03 -0500

    first commit using react-mdl, sidebar and header

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,36 +1,31 @@
 
 import React from 'react';
+import {Layout, Header, Textfield, Navigation, Drawer} from 'react-mdl';
 
 class App extends React.Component {
     render() {
         return (
-            <header id="header"
-                    className="demo-header mdl-layout__header mdl-color--grey-100 mdl-color-text--grey-600">
-                <div id="headerrow" className="mdl-layout__header-row">
-                    <button id="backbutton" className="mdl-button mdl-js-button mdl-button--icon" data-upgraded=",MaterialButton" style={{display: 'none', marginRight: 24}} onclick="getGraph()">
-                        <i className="material-icons">chevron_left</i>
-                    </button>
-                    <span id="home" className="mdl-layout-title">Home</span>
-                    <div className="mdl-layout-spacer" />
-                    <div className="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
-                        <label className="mdl-button mdl-js-button mdl-button--icon" htmlFor="search">
-                            <i className="material-icons">search</i>
-                        </label>
-                        <div className="mdl-textfield__expandable-holder">
-                            <input className="mdl-textfield__input" type="text" id="search" />
-                            <label className="mdl-textfield__label" htmlFor="search">Enter your query...</label>
-                        </div>
-                    </div>
-                    <button className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" id="hdrbtn">
-                        <i className="material-icons">more_vert</i>
-                    </button>
-                    <ul className="mdl-menu mdl-js-menu mdl-js-ripple-effect mdl-menu--bottom-right" htmlFor="hdrbtn">
-                        <li className="mdl-menu__item">About</li>
-                        <li className="mdl-menu__item">Contact</li>
-                        <li className="mdl-menu__item">Legal information</li>
-                    </ul>
-                </div>  
-            </header>
+            <div style={{height: '300px', position: 'relative'}}>
+                <Layout fixedHeader fixedDrawer>
+                    <Header title="Title" style={{backgroundColor: 'rgb(245,245,245)', color: 'rgb(117,117,117)'}}>
+                        <Textfield
+                            value=""
+                            onChange={() => {}}
+                            label="Search"
+                            expandable
+                            expandableIcon="search"
+                        />
+                    </Header>
+                    <Drawer title="Title" style={{color: 'rgb(236,239,241)', backgroundColor: 'rgb(38,50,56)'}}>
+                        <Navigation>
+                            <a href="">Link</a>
+                            <a href="">Link</a>
+                            <a href="">Link</a>
+                            <a href="">Link</a>
+                        </Navigation>
+                    </Drawer>
+                </Layout>
+            </div>
         );
     }
 }

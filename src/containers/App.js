912a1900ccd914af3ff2f3ebedcc5c382aa4ad3d
Peter Stromhaug <pas282@cornell.edu>
2016-01-15 22:09:49 -0500

    first working mdl header

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,39 +1,38 @@
-// This file bootstraps the app with the boilerplate necessary
-// to support hot reloading in Redux
-import React, {PropTypes} from 'react';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import FuelSavingsApp from '../components/FuelSavingsApp';
-import * as FuelSavingsActions from '../actions/fuelSavingsActions';
+
+import React from 'react';
 
 class App extends React.Component {
-  render() {
-    const { fuelSavingsAppState, actions } = this.props;
-
-    return (
-        <FuelSavingsApp fuelSavingsAppState={fuelSavingsAppState} actions={actions} />
-    );
-  }
+    render() {
+        return (
+            <header id="header"
+                    className="demo-header mdl-layout__header mdl-color--grey-100 mdl-color-text--grey-600">
+                <div id="headerrow" className="mdl-layout__header-row">
+                    <button id="backbutton" className="mdl-button mdl-js-button mdl-button--icon" data-upgraded=",MaterialButton" style={{display: 'none', marginRight: 24}} onclick="getGraph()">
+                        <i className="material-icons">chevron_left</i>
+                    </button>
+                    <span id="home" className="mdl-layout-title">Home</span>
+                    <div className="mdl-layout-spacer" />
+                    <div className="mdl-textfield mdl-js-textfield mdl-textfield--expandable">
+                        <label className="mdl-button mdl-js-button mdl-button--icon" htmlFor="search">
+                            <i className="material-icons">search</i>
+                        </label>
+                        <div className="mdl-textfield__expandable-holder">
+                            <input className="mdl-textfield__input" type="text" id="search" />
+                            <label className="mdl-textfield__label" htmlFor="search">Enter your query...</label>
+                        </div>
+                    </div>
+                    <button className="mdl-button mdl-js-button mdl-js-ripple-effect mdl-button--icon" id="hdrbtn">
+                        <i className="material-icons">more_vert</i>
+                    </button>
+                    <ul className="mdl-menu mdl-js-menu mdl-js-ripple-effect mdl-menu--bottom-right" htmlFor="hdrbtn">
+                        <li className="mdl-menu__item">About</li>
+                        <li className="mdl-menu__item">Contact</li>
+                        <li className="mdl-menu__item">Legal information</li>
+                    </ul>
+                </div>  
+            </header>
+        );
+    }
 }
 
-App.propTypes = {
-  actions: PropTypes.object.isRequired,
-  fuelSavingsAppState: PropTypes.object.isRequired
-};
-
-function mapStateToProps(state) {
-  return {
-    fuelSavingsAppState: state.fuelSavingsAppState
-  };
-}
-
-function mapDispatchToProps(dispatch) {
-  return {
-    actions: bindActionCreators(FuelSavingsActions, dispatch)
-  };
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default App;

ff91424152779c9903359dee9bd5efd0a232b9b2
Peter Stromhaug <pas282@cornell.edu>
2016-01-28 00:24:47 -0500

    material-ui navbar

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,33 +1,34 @@
 
 import React from 'react';
 import {Layout, Header, Textfield, Navigation, Drawer} from 'react-mdl';
+import {LeftNav, AppBar} from 'material-ui';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
 
 class App extends React.Component {
+    _handleClick(e) {
+        e.preventDefault();
+
+        // Show/Hide the LeftMenu
+        console.log("click");
+        this.refs.leftNav.toggle();
+    }
+
     render() {
         return (
             <div style={{height: '300px', position: 'relative'}}>
-                <Layout fixedHeader fixedDrawer>
-                    <Header title="Title" style={{backgroundColor: 'rgb(245,245,245)', color: 'rgb(117,117,117)'}}>
-                        <Textfield
-                            value=""
-                            onChange={() => {}}
-                            label="Search"
-                            expandable
-                            expandableIcon="search"
-                        />
-                    </Header>
-                    <Drawer title="Title" style={{color: 'rgb(236,239,241)', backgroundColor: 'rgb(38,50,56)'}}>
-                        <Navigation>
-                            <a href="">Link</a>
-                            <a href="">Link</a>
-                            <a href="">Link</a>
-                            <a href="">Link</a>
-                        </Navigation>
-                    </Drawer>
-                </Layout>
+                <LeftNav
+                    ref="leftNav"
+                    docked={false}/>
+                <AppBar
+                    title="Title"
+                    iconClassNameRight="muidocs-icon-navigation-expand-more"
+                    onLeftIconButtonTouchTap={this._handleClick}
+                />
             </div>
         );
     }
 }
 
 export default App;
+
---
+++ src/containers/App2.js
@@ -0,0 +1,16 @@
+//
+//import React from 'react';
+//import AppBar from 'material-ui/lib/app-bar';
+//
+//class App extends React.Component {
+//    render() {
+//        return (
+//            <AppBar
+//                title="Title"
+//                iconClassNameRight="muidocs-icon-navigation-expand-more"
+//            />
+//        );
+//    }
+//}
+//
+//export default App;
